dbceca28ca7c38121566c6ac32a07f926cdffde3
Troy Griffiths <troygriffi@gmail.com>
2015-02-04 12:42:56 -0800

    Update Path.java

--- Path.java
+++ Path.java
@@ -2,8 +2,6 @@
 import java.util.List;
 
 public class Path {
-    // we use public fields fields here since this very simple class is
-    // used only for returning multiple results from shortestPath
     public final List<Vertex> vertices;
     public final int cost;
     
